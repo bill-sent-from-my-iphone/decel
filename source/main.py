c81cca9f18071170458db89fd07875cc51f16d0d
Charlie Dudzik <dudzikc@outlook.com>
2022-02-15 21:26:55 -0500

    Modified toml

--- source/main.py
+++ source/main.py
@@ -6,11 +6,12 @@ from view.sheet_window import SheetWindow
 from view.mainwindow import MainWindow
 from view.popup import Popup
 
-def main(args):
+def main():
+    args = sys.argv[1:]
+    main(args)
     m = MainWindow(args)
     m.loop()
 
 if __name__ == '__main__':
-    args = sys.argv[1:]
-    main(args)
+    main()
 
